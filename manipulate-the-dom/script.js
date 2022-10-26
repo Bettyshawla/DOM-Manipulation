a08624a2727c31401d455dc16e285fd932ac300c
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-10-25 21:40:58 -0400

    final commit

--- manipulate-the-dom/script.js
+++ manipulate-the-dom/script.js
@@ -8,55 +8,110 @@ let quotes = [
 ];
 
 //=============Part 1===================
-
-document.querySelector('#main-title').innerHTML = 'My DOM manipulation'
-
+function mainTitle(x) {
+  document.querySelector('#main-title').innerHTML = x
+}
+mainTitle('Welcome to my page')
 //==============Part 2=================
-
-document.body.style.backgroundColor = 'pink'
-
+function backgroundColor(color) {
+  document.body.style.backgroundColor = color
+}
+backgroundColor('pink')
 //==============Part 3=================
-
-let removeLi = document.getElementById("favorite-things")
-removeLi.removeChild(removeLi.lastElementChild)
-
+function removeList() {
+  let removeLi = document.getElementById("favorite-things")
+  removeLi.removeChild(removeLi.lastElementChild)
+}
+removeList()
 //==============Part 4=================
 // Select all .special-title class elements and change their font-size to 2rem. Remember you might have to iterate through the list of elements
-
-let specialTitle= document.querySelector('.special-title')
-specialTitle.style.fontSize = '2rem'
-
+function specialFont(size) {
+  document.querySelectorAll('.special-title').forEach((title) => {
+    title.style.fontSize = size
+  });
+}
+specialFont('2rem')
 //==============Part 5=================
 // Turns out DOM never raced in Chicago. Access the Past Races list and remove Chicago.
 
 let pastRace = document.querySelector("#past-races")
-removeChicago = pastRace.getElementsByTagName ('li')[3]
+removeChicago = pastRace.getElementsByTagName('li')[3]
 pastRace.removeChild(removeChicago)
 
 // ==============Part 6=================
 // Let's add to DOM's Past Races list. Create a new <li> element, change the new <li> text to the name of a city, and append it to the Past Races list.
 
-let addSilverSpring = document.createElement('li')
-addSilverSpring.appendChild(document.createTextNode('Silver Spring'))
-pastRace.appendChild(addSilverSpring)
+function addCity(str) {
+  let addNewCity = document.createElement('li')
+  addNewCity.textContent = str
+  pastRace.prepend(addNewCity)
+}
+addCity("Silver Spring")
 
 //==============Part 7=================
 //You will have to create a new <div> with class of .blog-post, a new <h2> with text, and a new <p> with some text. Think about what order you want to create the elements, and what order you want to append them in.
 
 //addSilverSpring.classList.add('blog-post')
 
+function newBlogPost(h1, p) {
 
-const newDiv = document.createElement('div')
-newDiv.classList.add('blog-post', 'purple')
+  const newDiv = document.createElement('div')
+  newDiv.classList.add('blog-post', 'purple')
 
-let mainBlog = document.querySelector('.main')
-mainBlog.append(newDiv)
+  const mainBlog = document.querySelector('.main')
+  mainBlog.append(newDiv)
 
-const newH2 = document.createElement('h1')
-newH2.textContent = "Silver Spring";
+  const newH2 = document.createElement('h1')
+  newH2.textContent = h1;
 
-const newPar = document.createElement('p')
-newPar.textContent = "Family is the most important thing in this world!"
-newDiv.append(newH2,newPar)
+  const newPar = document.createElement('p')
+  newPar.textContent = p
+
+  newDiv.append(newH2, newPar)
+
+}
+newBlogPost("Silver Spring", "SILVER SPRING IS TOO CROWDED TO DRIVE WITH OVER SPEED!")
 
 
+//==============Part 8=================
+//Query select the #quote-title ID element and add a click event handler. That event handler should use the function randomQuotewhenever #quote-title is clicked.
+
+const randomQuote = function () {
+  document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+};
+// randomQuote()
+
+function chosenQuote(random) {
+  const selectQuote = document.querySelector(random);
+  selectQuote.addEventListener('click', function () {
+    randomQuote()
+  })
+}
+chosenQuote("#quote-title")
+
+//==============Part 9=================
+// Select all .blog-post class elements. Iterate through the list of .blog-post class elements and apply two event handlers to each node. The first event handler should be listening for mouseout events while the second handler should be listening for mouseenter events.
+
+// The mouseouthandler should toggle the class .purple
+// The mouseenterhandler should toggle the class .red
+
+function blogEventListner() {
+  const blogPost = document.getElementsByClassName('blog-post')
+  for (let i = 0; i < blogPost.length; i++) {
+    console.log(blogPost.item(i));
+    blogPost.item(i).onmouseout = function () {
+      if (blogPost.item(i).classList.contains('purple')) {
+        blogPost.item(i).classList.remove('purple');
+        blogPost.item(i).classList.add('red');
+      }
+    }
+    blogPost.item(i).onmouseenter = function () {
+      if (blogPost.item(i).classList.contains('red')) {
+        blogPost.item(i).classList.remove('red');
+        blogPost.item(i).classList.add('purple');
+      }
+    }
+
+  }
+}
+blogEventListner()
